870abb49f8c77da3d60bed0ce01241c72ccb9f7a
darius <dariusjackon11@gmail.com>
2025-11-09 16:36:25 +0300

    Update: About

---
+++ Frontend/src/Data/teamData.js
@@ -0,0 +1,20 @@
+export const team = [
+    {
+        name: "Darius Jackony",
+        role: "Frontend Developer",
+        description: "Builds user interfaces using React, HTML, and CSS.",
+        image: ""
+    },
+    {
+        name: "Ezama Steward",
+        role: "Photographer",
+        description: "Takes nice shorts",
+        image: ""
+    },
+    {
+        name: "Lubangakene Isaac",
+        role: "Frontend Developer",
+        description: "Builds user interfaces using React, HTML, and CSS.",
+        image: ""
+    }
+]
--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -1,9 +1,22 @@
+import { team } from "../Data/teamData";
+import "../Components/styles/about.css";
+function About() {
+  return (
+    <section>
+      <h2>Our Team</h2>
 
-function About(){
-    return(
-        <div>
-            <h1>Hallow, welcome to the About Section.</h1>
-        </div>
-    )
+      <div className="team-container">
+        {team.map((person, index) => (
+          <div key={index} className="team-member">
+            <img src={person.image} alt={person.name} />
+            <h3>{person.name}</h3>
+            <h4>{person.role}</h4>
+            <p>{person.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
+
 export default About;
